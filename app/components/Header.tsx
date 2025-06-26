33919cd0473b70edcb1736e6a952609f7e9d4529
rebelcl0ud <y0j0v33@gmail.com>
2025-06-26 04:00:00 -0600

    update: root + Header, toggle header based on auth

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,19 +1,26 @@
 import { Link } from "@remix-run/react";
 
-export default function Header() {
+type HeaderProps = {
+  userId: string | undefined;
+};
+
+export default function Header({ userId }: HeaderProps) {
   return (
     <div className="shrink-0 m-8">
       <Link className="p-4" to="/">
         Home
       </Link>
-      <Link className="p-4" to="/recipes">
-        Recipes
-      </Link>
       <Link className="p-4" to="/about">
         About
       </Link>
+      <Link className="p-4" to="/recipes">
+        Recipes
+      </Link>
+      <Link className="p-4" to="/dashboard">
+        {userId ? "Dashboard" : ""}
+      </Link>
       <Link className="p-4" to="/register">
-        Register
+        {userId ? "Logout" : "Login | Register"}
       </Link>
     </div>
   );
--- app/root.tsx
+++ app/root.tsx
@@ -1,12 +1,22 @@
-import { LinksFunction } from "@remix-run/node";
-import { Links, Meta, Outlet, Scripts } from "@remix-run/react";
+import { LinksFunction, LoaderFunctionArgs } from "@remix-run/node";
+import { Links, Meta, Outlet, Scripts, useLoaderData } from "@remix-run/react";
 import styles from "./tailwind.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import { getSession } from "./lib/sessions.server";
 
 export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
 
+export async function loader({ request }: LoaderFunctionArgs) {
+  const session = await getSession(request.headers.get("Cookie"));
+  const userId = session.get("userId");
+
+  return { userId };
+}
+
 export default function App() {
+  const { userId } = useLoaderData<typeof loader>();
+
   return (
     <html lang="en">
       <head>
@@ -15,7 +25,7 @@ export default function App() {
         <Links />
       </head>
       <body className="min-h-screen flex flex-col items-center">
-        <Header />
+        <Header userId={userId} />
         <Outlet />
         <Footer />
         <Scripts />
