d55b71c369109d060a418fbfdcb492aa5b853dbb
rebelcl0ud <y0j0v33@gmail.com>
2025-07-23 00:31:39 -0600

    add: register verification + user creation tests

--- tests/lib/auth.server.test.ts
+++ tests/lib/auth.server.test.ts
@@ -1,12 +1,17 @@
 import { describe, expect, test, vi } from "vitest";
 import { prisma } from "../../app/lib/prisma.server";
 import bcrypt from "bcrypt";
-import { validateCredentials } from "../../app/lib/auth.server";
+import {
+  checkIfUserExists,
+  createUser,
+  validateCredentials,
+} from "../../app/lib/auth.server";
 
 vi.mock("../../app/lib/prisma.server", () => ({
   prisma: {
     user: {
       findUnique: vi.fn(),
+      create: vi.fn(),
     },
   },
 }));
@@ -14,6 +19,7 @@ vi.mock("../../app/lib/prisma.server", () => ({
 vi.mock("bcrypt", () => ({
   default: {
     compare: vi.fn(),
+    hash: vi.fn(),
   },
 }));
 
@@ -36,5 +42,63 @@ describe("verify login", () => {
 
     const user = await validateCredentials("james.bond@mi6.com", "password123");
     expect(user).toEqual(testUser.id);
+  });
+});
+
+describe("verify registration", () => {
+  test("email already exists", async () => {
+    const testUser = {
+      id: Number("007"),
+      username: "friend",
+      email: "james.bond@mi6.com",
+      password: "hashed",
+      createdAt: fakeDate,
+      updatedAt: fakeDate,
+      deletedAt: null,
+    };
+
+    vi.spyOn(prisma.user, "findUnique").mockResolvedValue(testUser);
+
+    const user = await checkIfUserExists("james.bond@mi6.com");
+
+    expect(user).not.toBeNull();
+    expect(user!.errors.email[0]).toEqual("Email already registered.");
+  });
+
+  test("email does not exist, successful registration", async () => {
+    vi.spyOn(prisma.user, "findUnique").mockResolvedValue(null);
+
+    const user = await checkIfUserExists("james.bond@mi6.com");
+    expect(user).toBeNull();
+  });
+});
+
+describe("user creation", () => {
+  test("user creation, success", async () => {
+    const prismaSpy = vi.spyOn(prisma.user, "create").mockResolvedValue({
+      id: Number("007"),
+      username: "james",
+      email: "james.bond@mi6.com",
+      password: "hashed",
+      createdAt: fakeDate,
+      updatedAt: fakeDate,
+      deletedAt: null,
+    });
+
+    (bcrypt.hash as ReturnType<typeof vi.fn>).mockResolvedValue("hashed");
+
+    const user = await createUser("james.bond@mi6.com", "password", "james");
+
+    expect(bcrypt.hash).toHaveBeenCalledWith("password", 12);
+    expect(prismaSpy).toHaveBeenCalledWith({
+      data: {
+        email: "james.bond@mi6.com",
+        password: "hashed",
+        username: "james",
+      },
+    });
+    expect(user.email).toBe("james.bond@mi6.com");
+    expect(user.password).toBe("hashed");
+    expect(user.username).toBe("james");
   });
 });
