fb250114ffdb220dd395770b249d527872389fd9
rebelcl0ud <y0j0v33@gmail.com>
2025-07-24 23:14:04 -0600

    add: test coverage

--- vite.config.test.ts
+++ vite.config.test.ts
@@ -6,5 +6,29 @@ export default defineConfig({
     globals: true,
     environment: "jsdom",
     include: ["tests/**/*.test.{ts,tsx}"],
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "text-summary"],
+      exclude: [
+        "tests/**/*",
+        "*.test.ts",
+        "**/*.test.ts",
+        "public/",
+        "build/",
+        "types/",
+        "**/*.d.ts",
+        "prisma/",
+        "**/prisma.server.ts",
+        "**/server.ts",
+        "**/scripts/**",
+        // configs
+        "**/eslint.config.*",
+        "**/remix.config.*",
+        "**/vite.config.*",
+        //
+        "**/helpers.ts",
+      ],
+      reportsDirectory: "./tests/coverage",
+    },
   },
 });
